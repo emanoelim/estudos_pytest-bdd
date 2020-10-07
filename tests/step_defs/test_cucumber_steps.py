a0b02a882c805a4d6b5b507e9563d8267086f19c
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2020-10-07 20:13:43 -0300

    converters

--- tests/step_defs/test_cucumber_steps.py
+++ tests/step_defs/test_cucumber_steps.py
@@ -1,25 +1,34 @@
 import pytest
-from pytest_bdd import scenario, given, when, then
+from pytest_bdd import scenario, given, when, then, scenarios
 
 from cucumbers import CucumberBasket
 
 
-@scenario('../features/cucumbers.feature', 'Add cucumbers to a basket', example_converters=dict(start=int, add=int, total=int))
-def test_add():
-    # tudo que estiver aqui será exceutado após todos os passos do cenário
-    pass
+# @scenario('../features/cucumbers.feature', 'Add cucumbers to a basket', example_converters=dict(start=int, add=int, total=int))
+# def test_add():
+#     # tudo que estiver aqui será exceutado após todos os passos do cenário
+#     pass
+#
+#
+# @scenario('../features/cucumbers.feature', 'Add to many cucumbers to a basket', example_converters=dict(start=int, add=int, total=int))
+# def test_add_to_many():
+#     # tudo que estiver aqui será exceutado após todos os passos do cenário
+#     pass
+#
+#
+# @scenario('../features/cucumbers.feature', 'Remove cucumbers from a basket', example_converters=dict(start=int, remove=int, total=int))
+# def test_remove():
+#     # tudo que estiver aqui será exceutado após todos os passos do cenário
+#     pass
 
 
-@scenario('../features/cucumbers.feature', 'Add to many cucumbers to a basket', example_converters=dict(start=int, add=int, total=int))
-def test_add_to_many():
-    # tudo que estiver aqui será exceutado após todos os passos do cenário
-    pass
-
-
-@scenario('../features/cucumbers.feature', 'Remove cucumbers from a basket', example_converters=dict(start=int, remove=int, total=int))
-def test_remove():
-    # tudo que estiver aqui será exceutado após todos os passos do cenário
-    pass
+CONVERTERS = {
+    'start': int,
+    'add': int,
+    'remove': int,
+    'total': int
+}
+scenarios('../features/cucumbers.feature', example_converters=CONVERTERS)
 
 
 @pytest.fixture
